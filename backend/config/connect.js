90cab606e8dc5c9a6d127ef540881c9f66e998ce
RuiningKing <103382454+RuiningKing@users.noreply.github.com>
2023-07-01 17:15:07 +0300

    update

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log('Error connecting to MongoDB', err);
-  });
+const connectDB = async () => {
+  const development = JSON.parse(process.env.DEVELOPMENT);
+  const local = "mongodb://localhost:27017/marzone";
+  const remote = `mongodb+srv://kareemmoataz13:${process.env.MONGODB_PASSWORD}@cluster0.ptqqlb4.mongodb.net/?retryWrites=true&w=majority`;
+  const url = development ? local : remote;
 
-module.exports = mongoose;
+  try {
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log("Error connecting to MongoDB", err);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,26 @@ const express = require('express');
 const articleApi = require('./routes/article');
 const authorApi = require('./routes/author');
 const newsletterApi = require('./routes/newsletter');
-const req = require('express/lib/request');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  const development = JSON.parse(process.env.DEVELOPMENT);
+  const local = "mongodb://localhost:27017/marzone";
+  const remote = `mongodb+srv://kareemmoataz13:${process.env.MONGODB_PASSWORD}@cluster0.ptqqlb4.mongodb.net/?retryWrites=true&w=majority`;
+  const url = development ? local : remote;
 
-require('./config/connect');
+  try {
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log("Error connecting to MongoDB", err);
+  }
+};
+
+connectDB(); // Call the connectDB function to establish the database connection
 
 app.use(cors());
 app.use(express.json());
